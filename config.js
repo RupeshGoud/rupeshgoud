84035b789e3e478443fb697cd4a909fc542b6324
Rupesh Goud <iamrupeshgoud@gmail.com>
2019-12-01 17:23:57 +0530

    updated config

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 module.exports = {
   url: 'https://lumen.netlify.com',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
+  title: 'Blog by Rupesh Goud',
   subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
@@ -25,9 +25,9 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
+    name: 'Rupesh Goud',
     photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    bio: 'Programmer, Cricketer.',
     contacts: {
       email: '',
       facebook: '#',
